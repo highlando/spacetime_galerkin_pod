0539c63ae80edbcb1261db9f531b2c0a8e9e6eb3
highlando <jnhlnd@gmail.com>
2019-10-24 20:56:06 +0800

    now also testing gauss qr N=4

--- tests/test_units_gaussqr.py
+++ tests/test_units_gaussqr.py
@@ -5,20 +5,28 @@ import numpy as np
 
 class GaussQRTests(unittest.TestCase):
     def setUp(self):
-        self.Nlist = [1, 2, 3]
+        self.Nlist = [1, 2, 3, 4]
         self.absclist = [[.5],
                          [.5*(1-np.sqrt(1./3)), .5*(1+np.sqrt(1./3))],
-                         [.5*(1-np.sqrt(3./5)), .5, .5*(1+np.sqrt(3./5))]]
+                         [.5*(1-np.sqrt(3./5)), .5, .5*(1+np.sqrt(3./5))],
+                         [.5*(1-np.sqrt((15+2*np.sqrt(30))/35)),
+                          .5*(1-np.sqrt((15-2*np.sqrt(30))/35)),
+                          .5*(1+np.sqrt((15-2*np.sqrt(30))/35)),
+                          .5*(1+np.sqrt((15+2*np.sqrt(30))/35))]]
+
         self.wghtlist = [[1],
                          [.5, .5],
-                         [5./18, 8./18, 5./18]]
+                         [5./18, 8./18, 5./18],
+                         [(18-np.sqrt(30))/72, (18+np.sqrt(30))/72,
+                          (18+np.sqrt(30))/72, (18-np.sqrt(30))/72]]
 
     def test_gaussqr_uniform(self):
         for k, N in enumerate(self.Nlist):
             abscissae, weights = get_gaussqr_uniform(N)
-            # print(abscissae)
-            # print(self.absclist[k])
-            # print(weights)
+            print(abscissae)
+            print(self.absclist[k])
+            print(weights)
+            print(self.wghtlist[k])
             self.assertTrue(np.allclose(abscissae, np.array(self.absclist[k])))
             self.assertTrue(np.allclose(weights, self.wghtlist[k]))
 
