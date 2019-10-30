d171700163590e69e9ee10f42fed56b90539d2e1
highlando <jnhlnd@gmail.com>
2019-10-30 10:30:51 +0100

    file for tensor related things

---
+++ spacetime_galerkin_pod/ten_sor_utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+import scipy.sparse as sps
+
+import spacetime_galerkin_pod.gen_pod_utils as gpu
+
+
+def tnsrtrnsps(X, times=1):
+    '''transpose the tensor (by cycling the dimensions)'''
+    tnsdms = len(X.shape)
+    paxlist = np.arange(tnsdms).tolist()
+    paxlist.append(paxlist.pop(0))
+
+    for k in range(times):
+        X = np.transpose(X, paxlist)
+    return X
+
+
+def apply_mfacs_monemat(X, massfaclist):
+    lfac = massfaclist[2]
+    for clfac in massfaclist[2:]:
+        lfac = sps.kron(clfac, lfac)  # TODO: this will likely explode
+    return (massfaclist[0].T.dot(X)).dot(lfac)
+
+
+def modeone_massmats_svd(X, massfaclist, kdim):
+    Xdims = X.shape()
+    Xone = X.reshape((Xdims[0], -1))  # mode-1 matricization
+    mfXonemfs = apply_mfacs_monemat(Xone, massfaclist)
+    return gpu.get_ksvvecs(sol=mfXonemfs, poddim=kdim)
